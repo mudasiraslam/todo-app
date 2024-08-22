dd1e21c4a3770336bba041f214b474b84d6f4671
Muzamil <muhammadmuzamil5558@gmail.com>
2024-08-22 19:04:47 +0500

    add the suspenne in emal

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import React, { Suspense } from "react";
 import Link from "next/link";
 import { useVerifyEmail } from "../../hooks/useVerifyEmail";
 import { color } from "../constants/color";
@@ -8,30 +8,32 @@ function VerifyEmail() {
     const { verifed, error, verifyUserEmail } = useVerifyEmail();
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
-            <h1 className={`text-3xl  ${color.textVerified}`}> Verify Email</h1>
-            {
-                verifed ? (
-                    <div>
-                        <h2 className={`text-2xl  ${color.textVerified}`}> Email Verified</h2>
-                        <Link href={"/signin"}>
-                            <span className={` ${color.textSuccess}`}>Login</span>
-                        </Link>
-                    </div>
-                ) : error ? (
-                    <div className="mt-3">
-                        <h2 className={`text-2xl ${color.textUnverified}`}>{error}</h2>
-                    </div>
-                ) : (
-                    <button
-                        onClick={verifyUserEmail}
-                        className={`mt-4 px-4 py-2 font-semibold ${color.textSuccess} rounded`}
-                    >
-                        Click here
-                    </button>
-                )
-            }
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
+                <h1 className={`text-3xl  ${color.textVerified}`}> Verify Email</h1>
+                {
+                    verifed ? (
+                        <div>
+                            <h2 className={`text-2xl  ${color.textVerified}`}> Email Verified</h2>
+                            <Link href={"/signin"}>
+                                <span className={` ${color.textSuccess}`}>Login</span>
+                            </Link>
+                        </div>
+                    ) : error ? (
+                        <div className="mt-3">
+                            <h2 className={`text-2xl ${color.textUnverified}`}>{error}</h2>
+                        </div>
+                    ) : (
+                        <button
+                            onClick={verifyUserEmail}
+                            className={`mt-4 px-4 py-2 font-semibold ${color.textSuccess} rounded`}
+                        >
+                            Click here
+                        </button>
+                    )
+                }
+            </div>
+        </Suspense>
     );
 }
 
