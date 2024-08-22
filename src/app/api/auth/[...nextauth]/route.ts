9bbb31a45bf4f614587f63651e7d300aafdf3977
Muzamil <muhammadmuzamil5558@gmail.com>
2024-08-23 01:28:34 +0500

    first commit

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,6 @@
-// import { authOptions } from "../../../../libs/AuthOptions";
-// import NextAuth from "next-auth/next";
+import { authOptions } from "../../../../libs/AuthOptions";
+import NextAuth from "next-auth/next";
 
-// const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
-// export { handler as GET, handler as POST }
-
-
-import prismadb from "../../../../libs/prismadb";
-
-export const authOptions = {
-    providers: [
-
-    ],
-    callbacks: {
-        async session({ session, token, user }: any) {
-
-            const dbUser = await prismadb.user.findUnique({
-                where: { email: session.user.email },
-            });
-            session.user.id = dbUser?.id;
-            return session;
-        },
-    },
-};
+export { handler as GET, handler as POST };
