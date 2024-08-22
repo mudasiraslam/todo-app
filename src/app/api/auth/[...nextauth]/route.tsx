837f0f6ef1d7c5d06c3b15470d97f1e1421fec5b
Muzamil <muhammadmuzamil5558@gmail.com>
2024-08-23 00:19:50 +0500

    first commit

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,41 +1,7 @@
-// import { authOptions } from "../../../../libs/AuthOptions";
-// import NextAuth from "next-auth/next";
-
-// const handler = NextAuth(authOptions);
-
-// export {handler as GET, handler as POST}
-
-// import NextAuth from "next-auth/next";
-// import { authOptions } from "../../../../libs/AuthOptions";
-
-// export const handler = NextAuth(authOptions);
-
-// export { handler as GET, handler as POST };
-
-// handler.catch((error: any) => {
-//     console.error("Error during NextAuth request handling:", error);
-//     throw error;
-// });
-
-import NextAuth from "next-auth/next";
 import { authOptions } from "../../../../libs/AuthOptions";
+import NextAuth from "next-auth/next";
 
-export const handler = NextAuth({
-    ...authOptions,
-    callbacks: {
-        ...authOptions.callbacks,
-        async signIn({ user, account, profile }) {
-            try {
+const handler = NextAuth(authOptions);
 
-                return true;
-            } catch (error) {
-                console.error("Error during sign-in:", error);
-                return false;
-            }
-        },
-    },
-});
-
-export { handler as GET, handler as POST };
-
+export { handler as GET, handler as POST }
 
