1f6115f8d633b5c867a20577b10bc2596e1ee624
Muzamil <muhammadmuzamil5558@gmail.com>
2024-08-23 01:18:03 +0500

    first commit

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,3 +1,12 @@
+// import { authOptions } from "../../../../libs/AuthOptions";
+// import NextAuth from "next-auth/next";
+
+// const handler = NextAuth(authOptions);
+
+// export { handler as GET, handler as POST }
+
+
+
 import { authOptions } from "../../../../libs/AuthOptions";
 import NextAuth from "next-auth/next";
 
