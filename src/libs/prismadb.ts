efc5808f62753ac6d731a2231514074b12ab19cb
Muzamil <muhammadmuzamil5558@gmail.com>
2024-08-23 01:50:08 +0500

    first commit

--- src/libs/prismadb.ts
+++ src/libs/prismadb.ts
@@ -1,4 +1,4 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 // declare global {
 //   var prisma: PrismaClient;
@@ -10,14 +10,12 @@
 
 // export default client;
 
-import { PrismaClient } from "@prisma/client";
-
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient;
 }
 
-const client = globalThis.prisma || new PrismaClient();
+const prisma = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
-export default client;
+export default prisma;
