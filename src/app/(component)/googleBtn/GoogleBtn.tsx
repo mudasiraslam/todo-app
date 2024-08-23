47e940764a6987bd1644a79afe2aa42a3fefb427
Muzamil <muhammadmuzamil5558@gmail.com>
2024-08-24 00:24:26 +0500

    first commit

--- src/app/(component)/googleBtn/GoogleBtn.tsx
+++ src/app/(component)/googleBtn/GoogleBtn.tsx
@@ -5,13 +5,15 @@ import Image from 'next/image'
 
 
 function GoogleBtn() {
-    const signInWithGoogle = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        signIn("google", { callbackUrl: " http://localhost:3000/" })
-    }
+    // const signInWithGoogle = (e: React.MouseEvent<HTMLButtonElement>) => {
+    //     e.preventDefault();
+    //     signIn("google", { callbackUrl: process.env.NEXTAUTH_URL })
+
+
+    // }
     return (
         <>
-            <button className=' text-orange-500 w-full font-plex-mono mt-9 bg-[#232020] flex items-center p-2 border-customOrange border-2 rounded-[30px]' onClick={signInWithGoogle}>
+            <button className=' text-orange-500 w-full font-plex-mono mt-9 bg-[#232020] flex items-center p-2 border-customOrange border-2 rounded-[30px]' onClick={ }>
                 <Image src={GoogleImg} className='w-[28px] h-[28px]  ml-2' alt='Google logo' />
                 <span className='ml-2'>Sign Up with Google</span>
             </button>
--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic';
 
 
 const VerifyEmail = dynamic(() => import('../../app/(component)/verify-email/verify-email'), {
-    ssr: false, // Ensures that the component is only rendered on the client-side
+    ssr: false,
 });
 
 export default function Page() {
