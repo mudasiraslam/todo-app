9e5ea596b714a93e10d982e680d8e6bad10cf2be
Muzamil <muhammadmuzamil5558@gmail.com>
2024-08-23 16:02:19 +0500

    first commit

--- src/app/api/changePassword/route.tsx
+++ src/app/api/changePassword/route.tsx
@@ -1,15 +1,53 @@
+// import { NextRequest, NextResponse } from "next/server";
+// import prismadb from "../../../libs/prismadb";
+// import bcrypt from "bcrypt";
+// import { getServerSession } from "next-auth/next";
+// import { authOptions } from "../../../libs/AuthOptions";
+
+// export async function POST(request: NextRequest) {
+//     try {
+//         const reqBody = await request.json();
+//         const { newPassword } = reqBody;
+
+
+//         const session = await getServerSession(authOptions);
+//         if (!session || !session.user?.email) {
+//             return new NextResponse("Unauthorized", { status: 401 });
+//         }
+
+//         const user = await prismadb.user.findFirst({
+//             where: { email: session.user.email },
+//         });
+
+//         if (!user) {
+//             return new NextResponse("User not found", { status: 404 });
+//         }
+
+//         const hashedNewPassword = await bcrypt.hash(newPassword, 12);
+
+//         await prismadb.user.update({
+//             where: { id: user.id },
+//             data: { passwordHash: hashedNewPassword },
+//         });
+
+//         return new NextResponse("Password changed successfully", { status: 200 });
+//     } catch (error: any) {
+//         console.error("CHANGE_PASSWORD_ERROR:", error);
+//         return new NextResponse("An error occurred while changing the password", { status: 500 });
+//     }
+// }
+
+
 import { NextRequest, NextResponse } from "next/server";
 import prismadb from "../../../libs/prismadb";
 import bcrypt from "bcrypt";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../../../libs/AuthOptions";
-import { ApiResponse } from "@/app/type/type.todo";
 
 export async function POST(request: NextRequest) {
     try {
         const reqBody = await request.json();
         const { newPassword } = reqBody;
-
 
         const session = await getServerSession(authOptions);
         if (!session || !session.user?.email) {
@@ -21,8 +59,7 @@ export async function POST(request: NextRequest) {
         });
 
         if (!user) {
-            // return new NextResponse("User not found", { status: 404 });
-            return new NextResponse<ApiResponse>("User not found", { status: 404 });
+            return new NextResponse("User not found", { status: 404 });
         }
 
         const hashedNewPassword = await bcrypt.hash(newPassword, 12);
@@ -32,10 +69,9 @@ export async function POST(request: NextRequest) {
             data: { passwordHash: hashedNewPassword },
         });
 
-        return new NextResponse<ApiResponse>("Password changed successfully", { status: 200 });
+        return NextResponse.json({ message: "Password changed successfully" }, { status: 200 });
     } catch (error: any) {
-        // console.error("CHANGE_PASSWORD_ERROR:", error);
-        // return new NextResponse("An error occurred while changing the password", { status: 500 });
+        console.error("CHANGE_PASSWORD_ERROR:", error);
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
