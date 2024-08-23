20cfe6add761f9af7af4427972225f9ca597ded3
Muzamil <muhammadmuzamil5558@gmail.com>
2024-08-23 12:14:24 +0500

    first commit

--- src/app/api/changeEmail/route.tsx
+++ src/app/api/changeEmail/route.tsx
@@ -1,56 +1,13 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import prisma from '../../../libs/prismadb';
-
-
-// export async function POST(request: NextRequest) {
-//     try {
-//         const reqBody = await request.json();
-//         const { token, email } = reqBody;
-//         if (!token || !email) {
-//             return NextResponse.json("Email missing", { status: 400 });
-//         }
-
-//         const user = await prisma.user.findFirst({
-//             where: {
-//                 emailChangeToken: token,
-//                 emailChangeExpiry: {
-//                     gt: new Date(Date.now()),
-//                 },
-//             },
-//         });
-//         if (!user) {
-//             return NextResponse.json('Invaild Token', { status: 400 });
-//         }
-
-//         await prisma.user.update({
-//             where: { id: user.id },
-//             data: {
-//                 email: email,
-//                 emailChangeToken: null,
-//                 emailChangeExpiry: null,
-//             },
-//         });
-
-//         return NextResponse.json("Email Change succefully", {
-//             status: 201,
-//         },
-//         );
-//     } catch (error: any) {
-//         return NextResponse.json({ error: error.message }, { status: 500 });
-//     }
-// }
-
-
 import { NextRequest, NextResponse } from "next/server";
 import prisma from '../../../libs/prismadb';
+
 
 export async function POST(request: NextRequest) {
     try {
         const reqBody = await request.json();
         const { token, email } = reqBody;
-
         if (!token || !email) {
-            return NextResponse.json({ message: "Token or email missing" }, { status: 400 });
+            return NextResponse.json("Email missing", { status: 400 });
         }
 
         const user = await prisma.user.findFirst({
@@ -61,9 +18,8 @@ export async function POST(request: NextRequest) {
                 },
             },
         });
-
         if (!user) {
-            return NextResponse.json({ message: 'Invalid Token or Expired' }, { status: 400 });
+            return NextResponse.json('Invaild Token', { status: 400 });
         }
 
         await prisma.user.update({
@@ -75,9 +31,11 @@ export async function POST(request: NextRequest) {
             },
         });
 
-        return NextResponse.json({ message: "Email changed successfully" }, { status: 201 });
+        return NextResponse.json("Email Change succefully", {
+            status: 201,
+        },
+        );
     } catch (error: any) {
-        console.error("CHANGE_EMAIL_ERROR:", error);
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
