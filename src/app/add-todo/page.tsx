ae0563109c4e17d0ee950ef9505f8806e674de63
Muzamil <muhammadmuzamil5558@gmail.com>
2024-08-24 03:26:02 +0500

    first commit

--- src/app/add-todo/page.tsx
+++ src/app/add-todo/page.tsx
@@ -1,9 +1,9 @@
-// app/add-todo/page.tsx (or similar, based on your file structure)
+
 import dynamic from 'next/dynamic';
 
-// Dynamically import the AddTodo component with no server-side rendering
+
 const AddTodo = dynamic(() => import('../(component)/mainList/addTodo'), {
-    ssr: false, // Ensures that the component is only rendered on the client-side
+    ssr: false,
 });
 
 export default function Page() {
