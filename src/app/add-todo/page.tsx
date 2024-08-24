734052ebc3d7bb0359c99f457ae4afb39a944c6a
Muzamil <muhammadmuzamil5558@gmail.com>
2024-08-24 19:25:14 +0500

    first commit

--- src/app/add-todo/page.tsx
+++ src/app/add-todo/page.tsx
@@ -1,7 +1,15 @@
 
 import dynamic from 'next/dynamic';
+import { Metadata } from 'next';
 
-
+export const metadata: Metadata = {
+    title: "Settings Page",
+    description: "Change your Profile Pic, Name, Email, and Password.",
+    openGraph: {
+        title: "Settings Page - Todo App",
+        description: "Change your Profile Pic, Name, Email, and Password.",
+    },
+};
 const AddTodo = dynamic(() => import('../(component)/mainList/addTodo'), {
     ssr: false,
 });
